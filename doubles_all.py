d412b70afb97a018d068ab25b5031b80cda24f23
Victor Shyba <victor1984@riseup.net>
2017-11-18 01:36:54 -0300

    add a numba example

--- doubles_all.py
+++ doubles_all.py
@@ -9,6 +9,18 @@ import sys
 sys.path.append('./pyext-myclib')
 
 import myclib  # <-- Importing C Implemented Library
+from numba import jit
+
+@jit(nopython=True)
+def count_doubles_once_numba(val):
+    total = 0
+    chars = iter(val)
+    c1 = next(chars)
+    for c2 in chars:
+        if c1 == c2:
+            total += 1
+        c1 = c2
+    return total
 
 
 def count_doubles(val):
@@ -65,6 +77,10 @@ def test_pure_python(benchmark):
 
 def test_pure_python_once(benchmark):
     print(benchmark(count_doubles_once, val))
+
+
+def test_pure_python_once_numba(benchmark):
+    print(benchmark(count_doubles_once_numba, val.encode()))
 
 
 def test_itertools(benchmark):
